30fb2bab8b2df482b9f99258349373037092f67e
liqiao <liqiao19880312@126.com>
2020-03-06 23:06:55 +0800

    提交springboot项目到本地仓库

---
+++ springboot_quick/src/main/java/com/liqiao/controller/QuickController.java
@@ -0,0 +1,13 @@
+package com.liqiao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuickController {
+
+    @RequestMapping("/quick")
+    public String quickController() {
+       return "hello springboot!!!1111";
+    }
+}
